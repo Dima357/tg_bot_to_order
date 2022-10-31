8568929e7baebce5545bcb54bfd99be25b4a344a
Dima357 <110188693+Dima357@users.noreply.github.com>
2022-10-31 16:45:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("TOKEN")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    home_photo = open('floor/home_photo.jpeg', 'rb')
+    markup = types.InlineKeyboardMarkup()
+    help = types.InlineKeyboardButton('Помощь', callback_data='Помощь')
+    choice = types.InlineKeyboardButton('Виды полов', callback_data='Виды полов')
+    basket = types.InlineKeyboardButton('Корзина', callback_data='Корзина')
+    markup.add(help, choice, basket)
+    try:
+        bot.edit_message_caption(chat_id=message.chat.id, message_id=message.id,
+                                 caption='', reply_markup=markup)
+    except telebot.apihelper.ApiTelegramException:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+        bot.send_photo(message.chat.id, home_photo, reply_markup=markup, parse_mode='html')
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data)
+def check_callback_data(callback):
+    if callback.data == 'Меню':
+        start(callback.message)
+    elif callback.data == 'Помощь':
+        help(callback.message)
+    elif callback.data == 'Виды полов':
+        choice(callback.message)
+    elif callback.data == 'Корзина':
+        basket(callback.message)
+    elif callback.data == 'Оплатить заказ':
+        pay(callback.message)
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    home_photo = open('floor/home_photo.jpeg', 'rb')
+    greet = 'Бот для подбора теплых полов\n' \
+            'Вы можете указать текстом команды\n' \
+            'Например: меню, помощь, виды полов'
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    back = types.InlineKeyboardButton(text='<< Меню', callback_data='Меню')
+    markup.add(back)
+    try:
+        bot.edit_message_caption(chat_id=message.chat.id, message_id=message.id,
+                                 caption=greet, reply_markup=markup)
+    except telebot.apihelper.ApiTelegramException:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+        bot.send_photo(chat_id=message.chat.id, photo=home_photo, caption=greet, reply_markup=markup)
+
+
+@bot.message_handler(commands=['choice'])
+def choice(message):
+    home_photo = open('floor/home_photo.jpeg', 'rb')
+    greet = 'Виды полов:'
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    basket = types.InlineKeyboardButton(text='Корзина', callback_data='Корзина')
+    back = types.InlineKeyboardButton(text='<< Меню', callback_data='Меню')
+    markup.add(basket, back)
+    try:
+        bot.edit_message_caption(chat_id=message.chat.id, message_id=message.id,
+                                 caption=greet, reply_markup=markup)
+    except telebot.apihelper.ApiTelegramException:
+        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+        bot.send_photo(message.chat.id, home_photo, greet, reply_markup=markup)
+
+
+def basket(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    calculate_money = types.InlineKeyboardButton(text='Оплатить заказ', callback_data='Оплатить заказ')
+    back = types.InlineKeyboardButton(text='<< Меню', callback_data='Меню')
+    markup.add(calculate_money, back)
+    bot.edit_message_caption(chat_id=message.chat.id, message_id=message.id,
+                             caption='Корзина', reply_markup=markup)
+
+
+def pay(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    menu = types.InlineKeyboardButton(text='Меню', callback_data='Меню')
+    basket = types.InlineKeyboardButton(text='Корзина', callback_data='Корзина')
+    markup.add(basket, menu)
+    bot.edit_message_caption(chat_id=message.chat.id, message_id=message.id,
+                             caption='Оплата', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def text_from_user(message):
+    if 'меню' in message.text.lower():
+        start(message)
+    elif 'помощь' in message.text.lower():
+        help(message)
+    elif 'виды полов' in message.text.lower():
+        choice(message)
+    else:
+        answer = 'Проверьте правильность ввода\n'\
+                         'Возможные варианты\n' \
+                         'меню, помощь, виды полов'
+        bot.send_message(message.chat.id, answer)
+
+
+bot.infinity_polling()
